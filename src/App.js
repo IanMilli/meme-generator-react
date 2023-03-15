f3f4e7d9deecc693ebc49037182e3a61b6180dae
EDX-IL <117127605+EDX-IL@users.noreply.github.com>
2023-03-15 18:52:01 +0000

    added GalleryBar Component (coding to be done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Appbar from 'muicss/lib/react/appbar';
 import PhotoContainer from "./components/PhotoContainer.jsx";
 import Photo from "./components/Photo";
 import '../src/App.css';
+import GalleryBar from './components/GalleryBar.jsx';
 
 /**changed app.js from a functional component to make a class component */
 class App extends Component {
@@ -41,6 +42,11 @@ class App extends Component {
 
         <Appbar className='topAppBar'><h1>Meme Generator</h1></Appbar>;
         <div style={{ height: '30vw' }}>
+
+        {/* This section (not sure if section tags are needed) is where the 12 images (thumbnails) will display across the top below the navbar */}
+        <section>
+          <GalleryBar/>
+        </section>  
 
           <div className="photoContainer">
             <h2>some pics be here </h2>
