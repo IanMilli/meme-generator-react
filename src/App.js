e71b687dcba2f062356616621069bab2a8c6f357
manolisgi <114882721+manolisgi@users.noreply.github.com>
2023-03-15 19:50:31 +0000

    added the generate meme button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Appbar from 'muicss/lib/react/appbar';
 import Textarea from 'muicss/lib/react/textarea';
+import Button from 'muicss/lib/react/button';
 import PhotoContainer from "./components/PhotoContainer.jsx";
 import Photo from "./components/Photo";
 import '../src/App.css';
@@ -43,17 +44,19 @@ class App extends Component {
 
         <Appbar className='topAppBar'><h1>Meme Generator</h1></Appbar>;
         <div style={{ height: '30vw' }}>
- mui-text-box
-        <div>
+          mui-text-box
+          <div>
             <Textarea placeholder="Input 1" defaultValue="Top Text" />
             <Textarea placeholder="Input 2" defaultValue="Bottom Text" />
           </div>
+          <div>
+            <Button color="primary">Generate meme</Button>
+          </div>
 
-
-        {/* This section (not sure if section tags are needed) is where the 12 images (thumbnails) will display across the top below the navbar */}
-        <section>
-          <GalleryBar/>
-        </section>  
+          {/* This section (not sure if section tags are needed) is where the 12 images (thumbnails) will display across the top below the navbar */}
+          <section>
+            <GalleryBar />
+          </section>
 
 
           <div className="photoContainer">
