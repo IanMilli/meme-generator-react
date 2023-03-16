ad6f7bb48c0241a821ec7d6f789cd399ded4a2f4
IanMilli <120601739+IanMilli@users.noreply.github.com>
2023-03-16 19:22:04 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,19 +8,22 @@ import '../src/App.css';
 import GalleryBar from './components/GalleryBar.jsx';
 import PhotoSearch from "./components/PhotoSearch.jsx";
 import Panel from 'muicss/lib/react/panel';
+
 import ImagePick from './components/ImagePick';
+
 
 /**changed app.js from a functional component to make a class component */
 class App extends Component {
   render() {
     return (
       <div>
-        <Appbar className='topAppBar'><h1>Meme Generator</h1></Appbar>;
+        <Appbar className='topAppBar'><h1>Image Search</h1></Appbar>
         <div>
           <div>
             <Panel>
               <PhotoSearch></PhotoSearch>
             </Panel>
+            <Appbar className='topAppBar'><h1>Meme Generator</h1></Appbar>;
             <Textarea placeholder="Input 1" defaultValue="Top Text" />
             <Textarea placeholder="Input 2" defaultValue="Bottom Text" />
           </div>
@@ -29,7 +32,7 @@ class App extends Component {
           </div>
           {/* This section (not sure if section tags are needed) is where the 12 images (thumbnails) will display across the top below the navbar */}
           <section>
-            <GalleryBar />
+         
           </section>
         </div>
         <Appbar className='bottomAppBar'><a style={{ color: 'white' }} href="https://github.com/IanMilli/meme-generator-react">© 2023 Copyright: Manolis Giavasis, Ian Logendra & Ian
