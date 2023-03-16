f2c5a72cf912dc6151830adc5ba55042e52f0b05
Ian M <ianmillichamp.2@gmail.com>
2023-03-16 20:52:50 +0000

    css styling and appjs update

--- src/App.js
+++ src/App.js
@@ -14,6 +14,26 @@ import ImagePick from './components/ImagePick';
 
 /**changed app.js from a functional component to make a class component */
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      topText: '',
+      bottomText: ''
+    };
+
+    this.handleTopTextChange = this.handleTopTextChange.bind(this);
+    this.handleBottomTextChange = this.handleBottomTextChange.bind(this);
+  }
+
+  handleTopTextChange(event) {
+    this.setState({ topText: event.target.value });
+  }
+
+  handleBottomTextChange(event) {
+    this.setState({ bottomText: event.target.value });
+  }
+
   render() {
     return (
       <div>
@@ -23,16 +43,17 @@ class App extends Component {
             <Panel>
               <PhotoSearch></PhotoSearch>
             </Panel>
-            
-            <Textarea placeholder="Input 1" defaultValue="Top Text" />
-            <Textarea placeholder="Input 2" defaultValue="Bottom Text" />
           </div>
-          <div>
-            <Button color="primary">Generate meme</Button>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <Textarea  label="Top meme text" value={this.state.topText} onChange={this.handleTopTextChange} style={{ width: '30%' }} />
+            <Textarea  label="Bottom meme text" value={this.state.bottomText} onChange={this.handleBottomTextChange} style={{ width: '30%' }} />
+          </div>
+          <div style={{ textAlign: 'center' }}>
+            <Button color="primary" >Generate meme</Button>
           </div>
           {/* This section (not sure if section tags are needed) is where the 12 images (thumbnails) will display across the top below the navbar */}
           <section>
-         
+
           </section>
         </div>
         <Appbar className='bottomAppBar'><a style={{ color: 'white' }} href="https://github.com/IanMilli/meme-generator-react">© 2023 Copyright: Manolis Giavasis, Ian Logendra & Ian
