2fdc1fa2d907021d0d452689136805c6a5673011
Ian M <ianmillichamp.2@gmail.com>
2023-03-16 16:42:40 +0000

    added section devider between image search and meme generator

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import '../src/App.css';
 import GalleryBar from './components/GalleryBar.jsx';
 import PhotoSearch from "./components/PhotoSearch.jsx";
 import Panel from 'muicss/lib/react/panel';
+
 /**changed app.js from a functional component to make a class component */
 class App extends Component {
   render() {
@@ -16,12 +17,13 @@ class App extends Component {
       <div>
 
 
-        <Appbar className='topAppBar'><h1>Meme Generator</h1></Appbar>;
+        <Appbar className='topAppBar'><h1>Image Search</h1></Appbar>;
         <div>
           <div>
             <Panel>
               <PhotoSearch></PhotoSearch>
             </Panel>
+            <Appbar className='topAppBar'><h1>Meme Generator</h1></Appbar>;
             <Textarea placeholder="Input 1" defaultValue="Top Text" />
             <Textarea placeholder="Input 2" defaultValue="Bottom Text" />
           </div>
@@ -31,7 +33,7 @@ class App extends Component {
 
           {/* This section (not sure if section tags are needed) is where the 12 images (thumbnails) will display across the top below the navbar */}
           <section>
-            <GalleryBar />
+         
           </section>
 
 
