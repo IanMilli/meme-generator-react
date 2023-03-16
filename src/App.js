3dc6742ffb8be3b4f3cc6a9ae7dbba641793a4a5
IanMilli <120601739+IanMilli@users.noreply.github.com>
2023-03-16 15:46:00 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 
-
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Appbar from 'muicss/lib/react/appbar';
@@ -14,8 +13,6 @@ class App extends Component {
   render() {
     return (
       <div>
-
-
         <Appbar className='topAppBar'><h1>Meme Generator</h1></Appbar>;
         <div>
           <div>
@@ -28,15 +25,10 @@ class App extends Component {
           <div>
             <Button color="primary">Generate meme</Button>
           </div>
-
           {/* This section (not sure if section tags are needed) is where the 12 images (thumbnails) will display across the top below the navbar */}
           <section>
             <GalleryBar />
           </section>
-
-
-
-
         </div>
         <Appbar className='bottomAppBar'><a style={{ color: 'white' }} href="https://github.com/IanMilli/meme-generator-react">© 2023 Copyright: Manolis Giavasis, Ian Logendra & Ian
           Millichamp</a></Appbar>
