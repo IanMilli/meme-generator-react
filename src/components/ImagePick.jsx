aef1535090dfb5d02efe78a33ff6a35e423cfd50
Ian M <ianmillichamp.2@gmail.com>
2023-03-21 19:22:27 +0000

    scroll method to sections

--- src/components/ImagePick.jsx
+++ src/components/ImagePick.jsx
@@ -1,4 +1,4 @@
-import React, { useState ,} from "react";
+import React, { useState, } from "react";
 
 
 
@@ -11,55 +11,66 @@ import '../components/css/ImagePick.css';
 import TextOverPhoto from './TextOverPhoto';
 
 function ImagePick({ images }) {
+    // scroll to meme section when picture is clicked on
+    const handleClickScroll = () => {
+        const element = document.getElementById('section1');
+        if (element) {
+            //  Will scroll smoothly to the top of the next section
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
 
 
 
-    
-    const [selectedImage, setSelectedImage] = useState(null);
 
-    const handleImageClick = (imageSrc) => {
-        setSelectedImage(imageSrc);
+        const [selectedImage, setSelectedImage] = useState(null);
 
-    };
-      
-  
+        const handleImageClick = (imageSrc) => {
+            //calling function to scroll down page
+            handleClickScroll();
+            //calling function to set main pic for meme
+            setSelectedImage(imageSrc);
 
-    return (
-        <div>
+        };
 
-            <Row>
 
-                {images.map((image) => (
-                    <Col md='2' className="smallImageColumn" >
-                       
-                        <img
-                            className='smallImages'
-                            key={image.id}
-                            src={image.src.small}
-                            alt={image.photographer}
-                            onClick={() => handleImageClick(image.src.large)}
-                        />
+
+        return (
+            <div>
+
+                <Row>
+
+                    {images.map((image) => (
+                        <Col md='2' className="smallImageColumn" >
+
+                            <img
+                                className='smallImages'
+                                key={image.id}
+                                src={image.src.small}
+                                alt={image.photographer}
+                                onClick={() => handleImageClick(image.src.large)}
+                            />
+                        </Col>
+                    ))}
+
+                </Row>
+                <Panel className="mui--z5 panel">
+                    <Appbar className='topAppBar' id='section1'><h1>Meme Generator</h1></Appbar >
+                </Panel>
+                <Panel className="mui--z5 panel3">
+
+                    <Col className="mui--align-middle">
+
+
+
+                        <TextOverPhoto topText="Top Text Goes Here" photo={selectedImage} />
+
+
+
                     </Col>
-                ))}
+                </Panel>
+            </div>
+        );
+    }
 
-            </Row>
-            <Panel className="mui--z5 panel">
-            <Appbar className='topAppBar'><h1>Meme Generator</h1></Appbar>
-            </Panel>
-            <Panel className="mui--z5 panel3">
-               
-                <Col className="mui--align-middle">
-                
-                
-
-                     <TextOverPhoto topText="Top Text Goes Here" photo={selectedImage} />
-                
-                   
-                   
-                </Col>
-            </Panel>
-        </div>
-    );
-}
-
-export default ImagePick
+    export default ImagePick
